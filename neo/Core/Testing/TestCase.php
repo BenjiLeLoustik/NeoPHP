09e6cde6fe086d4c8cc9c53811225ae08caaa5d5
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-04-12 14:56:18 +0200

    add base TestCase for unit testing with container access

---
+++ neo/Core/Testing/TestCase.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Neo\Core\Testing;
+
+use Neo\App;
+use Neo\Core\DI\Container;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+abstract class TestCase extends PHPUnitTestCase
+{
+    protected Container $container;
+    protected static ?App $app = null;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        if (static::$app === null) {
+            static::$app = new App();
+        }
+
+        $this->container = static::$app->getContainer();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    protected function get(string $id): mixed
+    {
+        return $this->container->get($id);
+    }
+
+    protected function swap(string $id, mixed $value): void
+    {
+        $this->container->set($id, fn() => $value);
+    }
+}
