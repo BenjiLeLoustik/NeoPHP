b0f79ee3b1581d254ccb505d3a0bff10366ee5da
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-04-24 19:42:14 +0200

    feat: introduce UploadedFile abstraction for file uploads

---
+++ neo/Core/Http/File/UploadedFile.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Neo\Core\Http\File;
+
+class UploadedFile
+{
+    private array $file;
+
+    public function __construct(array $file)
+    {
+        $this->file = $file;
+    }
+
+    public function getOriginalName(): string
+    {
+        return $this->file['name'];
+    }
+
+    public function getTempPath(): string
+    {
+        return $this->file['tmp_name'];
+    }
+
+    public function getSize(): int
+    {
+        return $this->file['size'];
+    }
+
+    public function getError(): int
+    {
+        return $this->file['error'];
+    }
+
+    public function isValid(): bool
+    {
+        return $this->file['error'] === UPLOAD_ERR_OK;
+    }
+}
