2997fd4152d74ce5a6fe3617aa1aebdb19766b9d
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-05-16 09:03:47 +0200

    feat(profiler): define NEO_START_TIME and NEO_START_MEMORY constants

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+define('NEO_START_TIME',   microtime(true));
+define('NEO_START_MEMORY', memory_get_usage(true));
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Neo\App;
