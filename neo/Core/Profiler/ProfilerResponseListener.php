392e796ab13b31d8cd036d04a23e8931c40abd55
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-05-16 09:03:35 +0200

    feat(profiler): add ProfilerResponseListener to inject toolbar before </body>

---
+++ neo/Core/Profiler/ProfilerResponseListener.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace Neo\Core\Profiler;
+
+use Neo\Core\Event\Contract\EventSubscriberInterface;
+use Neo\Core\Event\Event\ResponseEvent;
+use Neo\Core\Http\Response\JsonResponse;
+use Neo\Core\Http\Response\RedirectResponse;
+use Neo\Core\Profiler\Toolbar\Toolbar;
+
+class ProfilerResponseListener implements EventSubscriberInterface
+{
+    public function __construct(private readonly Toolbar $toolbar) {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ResponseEvent::class => 'onResponse',
+        ];
+    }
+
+    public function onResponse(ResponseEvent $event): void
+    {
+        $response = $event->getResponse();
+
+        if ($response instanceof RedirectResponse || $response instanceof JsonResponse) {
+            return;
+        }
+
+        $contentType = $response->getHeaders()['Content-Type'] ?? 'text/html';
+        if (!str_contains($contentType, 'text/html')) {
+            return;
+        }
+
+        $content = $response->getContent();
+
+        $toolbar = $this->toolbar->render();
+        if (str_contains($content, '</body>')) {
+            $content = str_replace('</body>', $toolbar . '</body>', $content);
+        } else {
+            $content .= $toolbar;
+        }
+
+        $response->setContent($content);
+        $event->setResponse($response);
+    }
+}
