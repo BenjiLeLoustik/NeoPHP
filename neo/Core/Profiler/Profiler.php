b52b553d616e3dcacf42ad353873894bbf74a4e5
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-05-16 09:02:48 +0200

    feat(profiler): add Profiler singleton with collector aggregation

---
+++ neo/Core/Profiler/Profiler.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Neo\Core\Profiler;
+
+use Neo\Core\Profiler\Collector\CollectorInterface;
+
+class Profiler
+{
+    private static ?self $instance = null;
+    private array $collectors = [];
+    private float $startTime;
+    private int $startMemory;
+
+    private function __construct()
+    {
+        $this->startTime = defined('NEO_START_TIME')
+            ? NEO_START_TIME
+            : microtime(true);
+
+        $this->startMemory = defined('NEO_START_MEMORY')
+            ? NEO_START_MEMORY
+            : memory_get_usage(true);
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public static function reset(): void
+    {
+        self::$instance = null;
+    }
+
+    public function addCollector(CollectorInterface $collector): void
+    {
+        $this->collectors[$collector->getName()] = $collector;
+    }
+
+    public function getCollector(string $name): ?CollectorInterface
+    {
+        return $this->collectors[$name] ?? null;
+    }
+
+    public function getCollectors(): array
+    {
+        return $this->collectors;
+    }
+
+    public function getStartTime(): float
+    {
+        return $this->startTime;
+    }
+
+    public function getStartMemory(): int
+    {
+        return $this->startMemory;
+    }
+
+    public function getTotalDuration(): float
+    {
+        return (microtime(true) - $this->startTime) * 1000;
+    }
+
+    public function getPeakMemory(): int
+    {
+        return memory_get_peak_usage(true);
+    }
+
+    public function collect(): array
+    {
+        $data = [
+            'duration' => round($this->getTotalDuration(), 2),
+            'memory' => $this->getPeakMemory(),
+        ];
+
+        foreach ($this->collectors as $name => $collector) {
+            $data[$name] = $collector->collect();
+        }
+
+        return $data;
+    }
+}
