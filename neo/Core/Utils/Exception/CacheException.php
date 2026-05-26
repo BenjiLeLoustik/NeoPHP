9afb40540413642c35c6047a68096b83a747bbbe
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-05-26 19:01:12 +0200

    feat: add CacheException and ConfigException classes

---
+++ neo/Core/Utils/Exception/CacheException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Neo\Core\Utils\Exception;
+
+use Neo\Core\Error\Exception\FrameworkException;
+
+class CacheException extends FrameworkException
+{}
---
+++ neo/Core/Utils/Exception/ConfigException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Neo\Core\Utils\Exception;
+
+use Neo\Core\Error\Exception\FrameworkException;
+
+class ConfigException extends FrameworkException
+{}
