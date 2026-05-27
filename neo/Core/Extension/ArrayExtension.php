fa940fd48d79e4433f57070dc467334779daad78
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-05-27 18:38:13 +0200

    refactor: move StringExtension and ArrayExtension to Core/Extension/

--- neo/Core/Extension/ArrayExtension.php
+++ neo/Core/Extension/ArrayExtension.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Neo\Core\Utils;
+namespace Neo\Core\Extension;
 
 class ArrayExtension
 {
--- neo/Core/Extension/StringExtension.php
+++ neo/Core/Extension/StringExtension.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Neo\Core\Utils;
+namespace Neo\Core\Extension;
 
 use Neo\Core\DI\Container;
 use Neo\Core\View\View;
