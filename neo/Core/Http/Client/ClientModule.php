6ad9cbb8b9183a076c0cb566dc4cb570ed29b2a7
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-05-30 20:21:45 +0200

    refactor: register FlashViewExtension in ClientModule and tag as twig.extension

--- neo/Core/Http/Client/ClientModule.php
+++ neo/Core/Http/Client/ClientModule.php
@@ -6,6 +6,7 @@ namespace Neo\Core\Http\Client;
 use Neo\Core\DI\Container;
 use Neo\Core\Http\Client\Cookie\Cookie;
 use Neo\Core\Http\Client\Flash\Flash;
+use Neo\Core\Http\Client\Flash\FlashViewExtension;
 use Neo\Core\Http\Client\Session\Session;
 use Neo\Core\Http\HttpModule;
 use Neo\Core\Http\Request;
@@ -29,6 +30,9 @@ class ClientModule extends AbstractModule
         $container->set(Session::class, fn(Container $c) => new Session($c));
         $container->set(Cookie::class, fn(Container $c) => new Cookie($c));
         $container->set(Flash::class, fn(Container $c) => new Flash($c));
+
+        $container->set(FlashViewExtension::class, fn(Container $c) => new FlashViewExtension($c->get(Flash::class)));
+        $container->tag(FlashViewExtension::class, 'twig.extension');
     }
 
     protected function resolveDependencies(): void
