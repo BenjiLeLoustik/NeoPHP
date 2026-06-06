5892047f5b2bd4be6907a6a9585dd55f70a0d89a
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-06-06 15:27:13 +0200

    refactor(app): delegate detection and path registration to dedicated classes
    Replace inline getCurrentApplication() and registerBasePaths() calls with
    ApplicationDetector::detect() and ApplicationPaths::register(). App
    constructor is now solely responsible for bootstrapping.
    
    BREAKING CHANGE: detection and path logic no longer live in App.php.

--- neo/App.php
+++ neo/App.php
@@ -3,9 +3,10 @@ declare(strict_types=1);
 
 namespace Neo;
 
+use Neo\Core\Application\ApplicationDetector;
+use Neo\Core\Application\ApplicationPaths;
 use Neo\Core\Database\ORM\Model\AbstractModel;
 use Neo\Core\DI\Container;
-use Neo\Core\Error\Exception\FrameworkException;
 use Neo\Core\Event\Event\RequestEvent;
 use Neo\Core\Event\Event\ResponseEvent;
 use Neo\Core\Event\EventDispatcher;
@@ -14,7 +15,6 @@ use Neo\Core\Http\Request;
 use Neo\Core\Http\Response\Response;
 use Neo\Core\Module\ModuleManager;
 use Neo\Core\Routing\Router;
-use Neo\Core\Utils\Config\Config;
 
 class App
 {
@@ -25,14 +25,13 @@ class App
         $this->container = new Container();
         $this->container->set(Container::class, fn() => $this->container);
 
-        if (php_sapi_name() === 'cli') {
-            $this->container->set(Request::class, fn() => Request::createEmpty());
-        } else {
-            $this->container->set(Request::class, fn() => Request::fromGlobals());
-        }
+        $this->container->set(Request::class, fn() => php_sapi_name() === 'cli'
+            ? Request::createEmpty()
+            : Request::fromGlobals()
+        );
 
-        $this->getCurrentApplication();
-        $this->registerBasePaths();
+        (new ApplicationDetector($this->container))->detect();
+        (new ApplicationPaths($this->container))->register();
 
         (new ModuleManager($this->container))
             ->discover(__DIR__ . '/Core')
@@ -43,45 +42,6 @@ class App
                 ->enablePreviousUrlTracking(
                     $this->container->get(Session::class)
                 );
-        }
-    }
-
-    private function registerBasePaths(): void
-    {
-        $appName = $this->container->get('application');
-        $basePath = realpath(__DIR__ . '/../');
-
-        $this->container->set('basePath', $basePath);
-
-        if (is_dir($basePath . '/public_html')) {
-            $publicPath = realpath($basePath . '/public_html');
-        } elseif (is_dir($basePath . '/public')) {
-            $publicPath = realpath($basePath . '/public');
-        } else {
-            $publicPath = $basePath . '/public';
-        }
-
-        $this->container->set('publicPath', $publicPath);
-        $this->container->set('buildsPath', $publicPath . '/builds/');
-        $this->container->set('srcPath', $basePath . '/src');
-        $this->container->set('storagePath', $basePath . '/src/' . $appName . '/Storage');
-        $this->container->set('configsPath', $basePath . '/src/' . $appName . '/Config');
-        $this->container->set('viewsPath', $basePath . '/src/' . $appName . '/App/Views');
-        $this->container->set('controllersPath', $basePath . '/src/' . $appName . '/App/Controllers');
-        $this->container->set('assetsPath', $basePath . '/src/' . $appName . '/Assets/');
-        $this->container->set('repositoryPath', $basePath . '/src/' . $appName . '/Repository');
-        $this->container->set('modelPath', $basePath . '/src/' . $appName . '/Model');
-        $this->container->set('formPath', $basePath . '/src/' . $appName . '/App/Forms');
-        $this->container->set('listenersPath', $basePath . '/src/' . $appName . '/App/Event/Listener');
-        $this->container->set('cronsPath', $basePath . '/src/' . $appName . '/App/Crons');
-        $this->container->set('manifestFilename', 'manifest.json');
-        $this->container->set('controllerNamespace', 'Neo\\Src\\' . $appName . '\\App\\Controllers\\');
-        $this->container->set('modelNamespace', 'Neo\\Src\\' . $appName . '\\Model');
-        $this->container->set('repositoryNamespace', 'Neo\\Src\\' . $appName . '\\Repository');
-        $this->container->set('formNamespace', 'Neo\\Src\\' . $appName . '\\App\\Forms');
-
-        if (!empty($GLOBALS['_NEO_TEST_CONFIGS_PATH'])) {
-            $this->container->set('testConfigsPath', $GLOBALS['_NEO_TEST_CONFIGS_PATH']);
         }
     }
 
@@ -116,78 +76,6 @@ class App
         $responseEvent = new ResponseEvent($response);
         $dispatcher->dispatch($responseEvent);
         return $responseEvent->getResponse();
-    }
-
-    private function getCurrentApplication(): void
-    {
-        if (php_sapi_name() === 'cli') {
-            if (!empty($GLOBALS['_NEO_TEST_PROJECT'])) {
-                $this->container->set('application', $GLOBALS['_NEO_TEST_PROJECT']);
-                return;
-            }
-
-            global $argv;
-            $project = null;
-
-            foreach ($argv as $arg) {
-                if (str_starts_with($arg, '--project=')) {
-                    $project = substr($arg, strlen('--project='));
-                    break;
-                }
-            }
-
-            if (!$project) {
-                throw new FrameworkException(
-                    title: 'Application Error',
-                    message: "You must pass --project=ProjectName in CLI.",
-                    code: 500
-                );
-            }
-
-            $this->container->set('application', $project);
-            return;
-        }
-
-        $request = $this->container->get(Request::class);
-        $serverData = $request->getServer() ?? $_SERVER;
-
-        $serverName = $serverData['SERVER_NAME'] ?? $serverData['HTTP_HOST'] ?? null;
-        $serverPort = (string) ($serverData['SERVER_PORT'] ?? '');
-
-        if (!$serverName) {
-            throw new FrameworkException(
-                title: 'Application Error',
-                message: "Unable to detect the server name.",
-                code: 500
-            );
-        }
-
-        $server = $serverName;
-        if (!empty($serverPort) && !in_array($serverPort, ['80', '443'])) {
-            $server .= ':' . $serverPort;
-        }
-
-        foreach (glob(__DIR__ . '/../src/*/Config/app.config.php') as $file) {
-            $config       = require $file;
-            $accessServer = $config['access'] ?? null;
-
-            if ($accessServer === $server || $accessServer === $serverName) {
-                $this->container->set('application', basename(dirname($file, 2)));
-                return;
-            }
-        }
-
-        $projects = glob(__DIR__ . '/../src/*', GLOB_ONLYDIR);
-        if (count($projects) === 1) {
-            $this->container->set('application', basename($projects[0]));
-            return;
-        }
-
-        throw new FrameworkException(
-            title: 'Application Error',
-            message: sprintf("No application detected for access: '%s'.", $server),
-            code: 500
-        );
     }
 
     public function getContainer(): Container
