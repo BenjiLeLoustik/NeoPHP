cb8eeab234fa46ccb4750a763530fc56ad267945
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-06-21 14:58:15 +0200

    fix: phpstan level 6

--- neo/Core/Module/Tests/Fixture/ModuleCallLog.php
+++ neo/Core/Module/Tests/Fixture/ModuleCallLog.php
@@ -4,6 +4,9 @@ namespace Neo\Core\Module\Tests\Fixture;
 
 class ModuleCallLog
 {
+    /**
+     * @var list<string>
+     */
     public static array $calls = [];
 
     public static function reset(): void
