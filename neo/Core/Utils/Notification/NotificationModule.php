417caf0681af07debf70e4228f90ee8f4a89de41
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-06-21 07:36:12 +0200

    fix: remove comment and set register in DI et load resolveDependencies

--- neo/Core/Utils/Notification/NotificationModule.php
+++ neo/Core/Utils/Notification/NotificationModule.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 namespace Neo\Core\Utils\Notification;
 
 use Neo\Core\DI\Container;
+use Neo\Core\DI\Exception\ContainerException;
 use Neo\Core\Module\AbstractModule;
-use Neo\Core\Utils\Config\Config;
 
-/**
- * Module Notification.
- */
 class NotificationModule extends AbstractModule
 {
     public function dependencies(): array
@@ -19,16 +16,14 @@ class NotificationModule extends AbstractModule
 
     public function register(Container $container): void
     {
-        $container->singleton(NotificationManager::class, static function (Container $c): NotificationManager {
-            $config = $c->get(Config::class);
-            $notificationManager = new NotificationManager($c);
-
-            return $notificationManager;
-        });
+        $container->set(NotificationManager::class, fn(Container $c) => new NotificationManager($c));
     }
 
-    public function boot(Container $container): void
+    /**
+     * @throws ContainerException
+     */
+    protected function resolveDependencies(): void
     {
-        parent::boot($container);
+        $this->get(NotificationManager::class);
     }
 }
