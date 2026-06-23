ff1e829d6e257014c7d03260fe7406906ec4a7a4
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-06-23 09:50:16 +0200

    refactor: DatabaseControllerExtension expose entityManager property on controllers

--- neo/Core/Database/DatabaseControllerExtension.php
+++ neo/Core/Database/DatabaseControllerExtension.php
@@ -5,6 +5,7 @@ namespace Neo\Core\Database;
 
 use Neo\Core\Controller\AbstractController;
 use Neo\Core\Controller\Interface\ControllerExtensionInterface;
+use Neo\Core\Database\ORM\EntityManager;
 use Neo\Core\DI\Container;
 
 final class DatabaseControllerExtension implements ControllerExtensionInterface
@@ -12,5 +13,6 @@ final class DatabaseControllerExtension implements ControllerExtensionInterface
     public function extend(AbstractController $controller, Container $container): void
     {
         $controller->registerMethod('getDatabase', fn(): DatabaseFacade => new DatabaseFacade());
+        $controller->registerProperty('entityManager', fn(): EntityManager => $container->get(EntityManager::class));
     }
 }
