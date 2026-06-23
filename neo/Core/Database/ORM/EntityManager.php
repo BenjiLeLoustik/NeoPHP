7c79d096ba3fb12982a11ff79a69dd9f01aa8f0b
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-06-23 09:55:46 +0200

    fix: phpstan comment

--- neo/Core/Database/ORM/EntityManager.php
+++ neo/Core/Database/ORM/EntityManager.php
@@ -7,7 +7,6 @@ use Neo\Core\Database\Exception\DatabaseException;
 use Neo\Core\Database\ORM\Model\AbstractModel;
 use Neo\Core\Database\ORM\Repository\AbstractRepository;
 use Neo\Core\DI\Container;
-use Neo\Core\DI\Exception\ContainerException;
 
 class EntityManager
 {
@@ -21,19 +20,23 @@ class EntityManager
         $this->container = $container;
     }
 
+    /**
+     * @template T of AbstractModel
+     * @param class-string<T> $modelClass
+     * @return AbstractRepository<T>
+     * @throws DatabaseException
+     */
     public function getRepository(string $modelClass): AbstractRepository
     {
         if (isset($this->repositories[$modelClass])) {
+            /** @var AbstractRepository<T> */
             return $this->repositories[$modelClass];
         }
 
         if (!class_exists($modelClass) || !is_subclass_of($modelClass, AbstractModel::class)) {
             throw new DatabaseException(
                 title: 'Entity Manager Error',
-                message: sprintf(
-                    "'%s' is not a valid AbstractModel class.",
-                    $modelClass
-                ),
+                message: sprintf("'%s' is not a valid AbstractModel class.", $modelClass),
                 code: 500
             );
         }
@@ -47,6 +50,14 @@ class EntityManager
         return $this->repositories[$modelClass] = $repository;
     }
 
+    /**
+     * Allows manually binding a repository instance for a model class,
+     * useful for testing or for repositories with custom dependencies.
+     *
+     * @template T of AbstractModel
+     * @param class-string<T> $modelClass
+     * @param AbstractRepository<T> $repository
+     */
     public function setRepository(string $modelClass, AbstractRepository $repository): void
     {
         $this->repositories[$modelClass] = $repository;
@@ -58,25 +69,29 @@ class EntityManager
     }
 
     /**
-     * @throws ContainerException
+     * @param class-string<AbstractModel> $modelClass
+     * @return class-string
      */
     private function resolveRepositoryClass(string $modelClass): string
     {
         $parts = explode('\\', $modelClass);
         $modelName = array_pop($parts);
 
-        $repositoryClass = $this->container->get('repositoryNamespace');
+        $repositoryNamespace = $this->container->get('repositoryNamespace');
 
-        return rtrim($repositoryClass, '\\') . '\\' . $modelName . 'Repository';
+        return rtrim($repositoryNamespace, '\\') . '\\' . $modelName . 'Repository';
     }
 
     /**
-     * @throws ContainerException
+     * @template T of AbstractModel
+     * @param class-string $repositoryClass
+     * @param class-string<T> $modelClass
+     * @return AbstractRepository<T>
      */
     private function instantiate(string $repositoryClass, string $modelClass): AbstractRepository
     {
         if ($this->container->has($repositoryClass)) {
-            /** @var AbstractRepository $repository */
+            /** @var AbstractRepository<T> $repository */
             $repository = $this->container->get($repositoryClass);
             return $repository;
         }
