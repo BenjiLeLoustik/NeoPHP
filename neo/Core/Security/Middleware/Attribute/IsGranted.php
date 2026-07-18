f9b5867035e946ec0518cd620c764e06f303a520
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-18 10:13:57 +0200

    feat: add IsGranted attribute for multi-role OR checks

---
+++ neo/Core/Security/Middleware/Attribute/IsGranted.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Neo\Core\Security\Middleware\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+class IsGranted
+{
+    /**
+     * @param array<int, string> $roles
+     */
+    public function __construct(
+        public array $roles,
+        public string $message = '',
+        public string $onError = 'block',
+        public ?string $redirect = null,
+    ) {}
+}
