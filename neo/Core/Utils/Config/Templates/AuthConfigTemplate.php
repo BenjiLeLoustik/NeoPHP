429946156a3fa76bd108aeaec2695de99fa4fc63
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-18 16:59:45 +0200

    feat: add AuthConfigTemplate

---
+++ neo/Core/Utils/Config/Templates/AuthConfigTemplate.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace Neo\Core\Utils\Config\Templates;
+
+use Neo\Core\Utils\Config\Templates\Interface\ConfigTemplateInterface;
+
+class AuthConfigTemplate implements ConfigTemplateInterface
+{
+
+    public function filename(): string
+    {
+        return 'auth.config.php';
+    }
+
+    public function render(string $projectName, array $context = []): string
+    {
+        return <<<PHP
+<?php
+declare(strict_types=1);
+
+// ./src/$projectName/Config/auth.config.php
+
+return [
+    'enabled'    => false,
+    'model'      => '',
+    'identifier' => '',
+    'password'   => '',
+    'guard'      => 'session',
+    'role'       => [
+        'model'       => '',
+        'foreign_key' => '',
+        'field'       => '',
+    ],
+    'options' => [
+        'login'      => '',
+        'logout'     => '',
+        'home'       => '',
+        'secret'     => '',
+        'expiration' => 3600,
+        'timeout'    => 1800,
+        'algorithm'  => 'HS256',
+    ],
+];
+PHP;
+    }
+}
