05089bd71927752748b645d5e6cd1790ff5ab913
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-19 22:02:55 +0200

    refactor: move into Extension directory

--- neo/Core/Security/Middleware/Extension/MiddlewareControllerExtension.php
+++ neo/Core/Security/Middleware/Extension/MiddlewareControllerExtension.php
@@ -1,13 +1,14 @@
 <?php
 declare(strict_types=1);
 
-namespace Neo\Core\Security\Middleware;
+namespace Neo\Core\Security\Middleware\Extension;
 
 use Neo\Core\Controller\AbstractController;
 use Neo\Core\Controller\Interface\ControllerExtensionInterface;
 use Neo\Core\DI\Container;
 use Neo\Core\Extension\Attribute\Extension;
 use Neo\Core\Extension\Enum\ExtensionTypeEnum;
+use Neo\Core\Security\Middleware\MiddlewareManager;
 
 /**
  * @method \Neo\Core\Security\Middleware\MiddlewareManager getMiddleware()
