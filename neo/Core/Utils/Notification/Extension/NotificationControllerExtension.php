e7a8365c998f65fb23eae72075b0d9c9de903047
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-19 22:09:12 +0200

    refactor: move into Extension directory

--- neo/Core/Utils/Notification/Extension/NotificationControllerExtension.php
+++ neo/Core/Utils/Notification/Extension/NotificationControllerExtension.php
@@ -1,13 +1,14 @@
 <?php
 declare(strict_types=1);
 
-namespace Neo\Core\Utils\Notification;
+namespace Neo\Core\Utils\Notification\Extension;
 
 use Neo\Core\Controller\AbstractController;
 use Neo\Core\Controller\Interface\ControllerExtensionInterface;
 use Neo\Core\DI\Container;
 use Neo\Core\Extension\Attribute\Extension;
 use Neo\Core\Extension\Enum\ExtensionTypeEnum;
+use Neo\Core\Utils\Notification\NotificationManager;
 
 /**
  * @method \Neo\Core\Utils\Notification\NotificationManager getNotification()
