7d30d6ebe04210b991d60e437cb9b342f9b6d084
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-19 21:47:34 +0200

    refactor: move DatabaseControllerExtension and DatabaseViewExtension into Extension directory

--- neo/Core/Database/Extension/DatabaseControllerExtension.php
+++ neo/Core/Database/Extension/DatabaseControllerExtension.php
@@ -1,10 +1,11 @@
 <?php
 declare(strict_types=1);
 
-namespace Neo\Core\Database;
+namespace Neo\Core\Database\Extension;
 
 use Neo\Core\Controller\AbstractController;
 use Neo\Core\Controller\Interface\ControllerExtensionInterface;
+use Neo\Core\Database\DatabaseFacade;
 use Neo\Core\Database\ORM\EntityManager;
 use Neo\Core\DI\Container;
 use Neo\Core\Extension\Attribute\Extension;
