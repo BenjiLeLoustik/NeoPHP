add3f990b86facbbb44fce157f15a731cf015c41
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-19 21:43:31 +0200

    feat: remove tests

--- neo/Core/DI/Tests/Fixture/StubConcrete.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Neo\Core\DI\Tests\Fixture;
-
-final class StubConcrete implements StubInterface {}
--- neo/Core/Event/Tests/Fixture/ConcreteEvent.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Neo\Core\Event\Tests\Fixture;
-
-use Neo\Core\Event\Abstract\AbstractEvent;
-
-final class ConcreteEvent extends AbstractEvent {}
