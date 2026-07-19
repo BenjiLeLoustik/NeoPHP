0ca0e7dde9e4109ca39ba546974cba88ebf34bb1
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-19 21:43:31 +0200

    feat: remove tests

--- neo/Core/DI/Tests/Fixture/StubInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Neo\Core\DI\Tests\Fixture;
-
-interface StubInterface {}
--- neo/Core/Module/Tests/Fixture/Modules/Invalid/UnrelatedModule.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace Neo\Core\Module\Tests\Fixture\Modules\Invalid;
-
-class UnrelatedModule
-{}
