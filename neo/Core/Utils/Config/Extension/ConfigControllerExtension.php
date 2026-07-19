563087a1a1670e0e671657fd29af248e41f28417
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-19 22:06:21 +0200

    refactor: move into Extension directory

--- neo/Core/Utils/Config/Extension/ConfigControllerExtension.php
+++ neo/Core/Utils/Config/Extension/ConfigControllerExtension.php
@@ -1,13 +1,14 @@
 <?php
 declare(strict_types=1);
 
-namespace Neo\Core\Utils\Config;
+namespace Neo\Core\Utils\Config\Extension;
 
 use Neo\Core\Controller\AbstractController;
 use Neo\Core\Controller\Interface\ControllerExtensionInterface;
 use Neo\Core\DI\Container;
 use Neo\Core\Extension\Attribute\Extension;
 use Neo\Core\Extension\Enum\ExtensionTypeEnum;
+use Neo\Core\Utils\Config\ConfigManager;
 
 /**
  * @method \Neo\Core\Utils\Config\ConfigManager getConfig()
