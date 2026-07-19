9a0558f8fcd8a828158488c947b9ea9bcb58b7a2
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-19 22:07:40 +0200

    refactor: move into Extension directory

--- neo/Core/Utils/Logger/Extension/LoggerControllerExtension.php
+++ neo/Core/Utils/Logger/Extension/LoggerControllerExtension.php
@@ -1,13 +1,14 @@
 <?php
 declare(strict_types=1);
 
-namespace Neo\Core\Utils\Logger;
+namespace Neo\Core\Utils\Logger\Extension;
 
 use Neo\Core\Controller\AbstractController;
 use Neo\Core\Controller\Interface\ControllerExtensionInterface;
 use Neo\Core\DI\Container;
 use Neo\Core\Extension\Attribute\Extension;
 use Neo\Core\Extension\Enum\ExtensionTypeEnum;
+use Neo\Core\Utils\Logger\Logger;
 
 /**
  * @method \Neo\Core\Utils\Logger\Logger getLogger()
