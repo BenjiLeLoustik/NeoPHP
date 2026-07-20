8e58fa6e5297a545426999662c8e293d506ead08
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-20 08:08:30 +0200

    feat: add CsrfModule

---
+++ neo/Core/Security/Csrf/CsrfModule.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Neo\Core\Security\Csrf;
+
+use Neo\Core\DI\Container;
+use Neo\Core\DI\Exception\ContainerException;
+use Neo\Core\Http\Client\ClientModule;
+use Neo\Core\Http\Client\Session\Session;
+use Neo\Core\Http\HttpModule;
+use Neo\Core\Http\Request\Request;
+use Neo\Core\Module\Abstract\AbstractModule;
+
+class CsrfModule extends AbstractModule
+{
+    /**
+     * @return array<class-string>
+     */
+    public function dependencies(): array
+    {
+        return [
+            ClientModule::class,
+            HttpModule::class,
+        ];
+    }
+
+    public function register(Container $container): void
+    {
+        $container->set(CsrfManager::class, fn(Container $c) => new CsrfManager(
+            $c->get(Session::class),
+            $c->get(Request::class),
+        ));
+
+        $container->set(CsrfTokenManager::class, fn() => new CsrfTokenManager());
+    }
+
+    /**
+     * @throws ContainerException
+     */
+    protected function resolveDependencies(): void
+    {
+        $this->get(CsrfManager::class);
+        $this->get(CsrfTokenManager::class);
+    }
+}
