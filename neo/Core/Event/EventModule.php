bad913dfc645569c104f9d2d1b268c371ab66fb6
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-20 10:42:59 +0200

    refactor: convert EventModule to new module contract

--- neo/Core/Event/EventModule.php
+++ neo/Core/Event/EventModule.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 namespace Neo\Core\Event;
 
 use Neo\Core\DI\Container;
-use Neo\Core\Module\Abstract\AbstractModule;
+use Neo\Core\DI\Exception\ContainerException;
+use Neo\Core\Module\Interface\ModuleInterface;
+use Neo\Core\Utils\Config\ConfigModule;
 
-class EventModule extends AbstractModule
+class EventModule implements ModuleInterface
 {
     /**
      * @return list<class-string>
      */
     public function dependencies(): array
     {
-        return [];
+        return [
+            ConfigModule::class,
+        ];
     }
 
     public function register(Container $container): void
@@ -21,8 +25,11 @@ class EventModule extends AbstractModule
         $container->set(EventManager::class, fn(Container $c) => new EventManager($c));
     }
 
-    protected function resolveDependencies(): void
+    /**
+     * @throws ContainerException
+     */
+    public function init(Container $container): object
     {
-        $this->get(EventManager::class);
+        return $container->get(EventManager::class);
     }
 }
