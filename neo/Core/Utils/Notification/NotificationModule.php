110f51af5d25e0185f19185d4960f4b3ee855a8a
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-20 10:00:43 +0200

    refactor: convert to new module contract, remove cross-module imports

--- neo/Core/Utils/Notification/NotificationModule.php
+++ neo/Core/Utils/Notification/NotificationModule.php
@@ -5,13 +5,18 @@ namespace Neo\Core\Utils\Notification;
 
 use Neo\Core\DI\Container;
 use Neo\Core\DI\Exception\ContainerException;
-use Neo\Core\Module\Abstract\AbstractModule;
+use Neo\Core\Module\Interface\ModuleInterface;
+use Neo\Core\Utils\Config\ConfigModule;
+use Neo\Core\View\ViewModule;
 
-class NotificationModule extends AbstractModule
+class NotificationModule implements ModuleInterface
 {
     public function dependencies(): array
     {
-        return [];
+        return [
+            ConfigModule::class,
+            ViewModule::class,
+        ];
     }
 
     public function register(Container $container): void
@@ -22,8 +27,8 @@ class NotificationModule extends AbstractModule
     /**
      * @throws ContainerException
      */
-    protected function resolveDependencies(): void
+    public function init(Container $container): object
     {
-        $this->get(NotificationManager::class);
+        return $container->get(NotificationManager::class);
     }
 }
