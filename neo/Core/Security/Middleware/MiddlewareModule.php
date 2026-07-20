00283018ec2c53025ea803d0b97bff46bf30c890
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-20 08:10:42 +0200

    feat: add MiddlewareModule

---
+++ neo/Core/Security/Middleware/MiddlewareModule.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Neo\Core\Security\Middleware;
+
+use Neo\Core\DI\Container;
+use Neo\Core\DI\Exception\ContainerException;
+use Neo\Core\Http\Client\ClientModule;
+use Neo\Core\Http\HttpModule;
+use Neo\Core\Module\Abstract\AbstractModule;
+use Neo\Core\Routing\RouterModule;
+use Neo\Core\View\ViewModule;
+
+class MiddlewareModule extends AbstractModule
+{
+    /**
+     * @return array<class-string>
+     */
+    public function dependencies(): array
+    {
+        return [
+            HttpModule::class,
+            ClientModule::class,
+            RouterModule::class,
+            ViewModule::class,
+        ];
+    }
+
+    public function register(Container $container): void
+    {
+        $container->set(MiddlewareManager::class, fn(Container $c) => new MiddlewareManager($c));
+    }
+
+    /**
+     * @throws ContainerException
+     */
+    public function resolveDependencies(): void
+    {
+        $this->get(MiddlewareManager::class);
+    }
+}
