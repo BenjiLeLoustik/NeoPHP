9a1371bff1d24c719048a821bf8a2ebfd4782357
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-20 10:21:18 +0200

    refactor: convert CsrfModule to new module contract

--- neo/Core/Security/Csrf/CsrfModule.php
+++ neo/Core/Security/Csrf/CsrfModule.php
@@ -3,18 +3,12 @@
 namespace Neo\Core\Security\Csrf;
 
 use Neo\Core\DI\Container;
-use Neo\Core\DI\Exception\ContainerException;
 use Neo\Core\Http\Client\ClientModule;
-use Neo\Core\Http\Client\Session\Session;
-use Neo\Core\Http\HttpModule;
 use Neo\Core\Http\Request\Request;
-use Neo\Core\Module\Abstract\AbstractModule;
+use Neo\Core\Module\Interface\ModuleInterface;
 
-class CsrfModule extends AbstractModule
+class CsrfModule implements ModuleInterface
 {
-    /**
-     * @return array<class-string>
-     */
     public function dependencies(): array
     {
         return [
@@ -25,19 +19,17 @@ class CsrfModule extends AbstractModule
     public function register(Container $container): void
     {
         $container->set(CsrfManager::class, fn(Container $c) => new CsrfManager(
-            $c->get(Session::class),
+            $c->get('csrf.clientModule')->session(),
             $c->get(Request::class),
         ));
 
         $container->set(CsrfTokenManager::class, fn() => new CsrfTokenManager());
     }
 
-    /**
-     * @throws ContainerException
-     */
-    protected function resolveDependencies(): void
+    public function init(Container $container): object
     {
-        $this->get(CsrfManager::class);
-        $this->get(CsrfTokenManager::class);
+        $container->get(CsrfTokenManager::class);
+
+        return $container->get(CsrfManager::class);
     }
 }
