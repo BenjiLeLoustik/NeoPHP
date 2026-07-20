57c3f4c6ad4d601be823c56db8ee6fae231aa9cb
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-20 09:54:32 +0200

    refactor: convert ValidatorModule to new module contract

--- neo/Core/Validator/ValidatorModule.php
+++ neo/Core/Validator/ValidatorModule.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 namespace Neo\Core\Validator;
 
 use Neo\Core\DI\Container;
-use Neo\Core\Module\Abstract\AbstractModule;
+use Neo\Core\DI\Exception\ContainerException;
+use Neo\Core\Module\Interface\ModuleInterface;
 
-class ValidatorModule extends AbstractModule
+class ValidatorModule implements ModuleInterface
 {
     public function dependencies(): array
     {
@@ -16,5 +17,13 @@ class ValidatorModule extends AbstractModule
     public function register(Container $container): void
     {
         $container->set(ValidatorManager::class, fn() => new ValidatorManager());
+    }
+
+    /**
+     * @throws ContainerException
+     */
+    public function init(Container $container): object
+    {
+        return $container->get(ValidatorManager::class);
     }
 }
