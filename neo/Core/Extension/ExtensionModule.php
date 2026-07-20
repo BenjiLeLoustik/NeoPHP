cfd91a9e7bd01b3bd327b46d24d99e7d967592d2
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-20 10:35:56 +0200

    refactor: convert ExtensionModule to new module contract

--- neo/Core/Extension/ExtensionModule.php
+++ neo/Core/Extension/ExtensionModule.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 namespace Neo\Core\Extension;
 
 use Neo\Core\DI\Container;
-use Neo\Core\Module\Abstract\AbstractModule;
+use Neo\Core\DI\Exception\ContainerException;
+use Neo\Core\Module\Interface\ModuleInterface;
 
-final class ExtensionModule extends AbstractModule
+final class ExtensionModule implements ModuleInterface
 {
     public function dependencies(): array
     {
@@ -16,5 +17,13 @@ final class ExtensionModule extends AbstractModule
     public function register(Container $container): void
     {
         $container->set(ExtensionManager::class, fn(Container $c) => new ExtensionManager($c));
+    }
+
+    /**
+     * @throws ContainerException
+     */
+    public function init(Container $container): object
+    {
+        return $container->get(ExtensionManager::class);
     }
 }
