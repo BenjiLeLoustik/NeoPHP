416f8c7b2b40e36b3be3c316bfd055504f081965
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-30 07:19:24 +0200

    feat: add MarkdownModule
    Register MarkdownManager in the container and declare ViewModule as a
    dependency so the Markdown view extension is auto-discovered.
    Implements ModuleInterface (dependencies/register/init) like AssetModule.

---
+++ neo/Core/Tools/Markdown/MarkdownModule.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Neo\Core\Tools\Markdown;
+
+use Neo\Core\DI\Container;
+use Neo\Core\DI\Exception\ContainerException;
+use Neo\Core\Module\Interface\ModuleInterface;
+use Neo\Core\View\ViewModule;
+
+class MarkdownModule implements ModuleInterface
+{
+
+    /**
+     * @return list<class-string>
+     */
+    public function dependencies(): array
+    {
+        return [
+            ViewModule::class,
+        ];
+    }
+
+    public function register(Container $container): void
+    {
+        $container->set(MarkdownManager::class, fn(Container $container) => new MarkdownManager($container));
+    }
+
+    /**
+     * @throws ContainerException
+     */
+    public function init(Container $container): object
+    {
+        return $container->get(MarkdownManager::class);
+    }
+}
