24f8469e5f375660368e8860bd7a1e4c82995d79
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-30 08:14:17 +0200

    feat: register HttpClient module
    Register HttpClientManager in the container and bind HttpClientInterface
    to it so controllers can inject the interface directly.

---
+++ neo/Core/Http/HttpClient/HttpClientModule.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Neo\Core\Http\HttpClient;
+
+use Neo\Core\DI\Container;
+use Neo\Core\DI\Exception\ContainerException;
+use Neo\Core\Http\HttpClient\Interface\HttpClientInterface;
+use Neo\Core\Module\Interface\ModuleInterface;
+
+final class HttpClientModule implements ModuleInterface
+{
+    /**
+     * @return list<class-string>
+     */
+    public function dependencies(): array
+    {
+        return [];
+    }
+
+    public function register(Container $container): void
+    {
+        $container->set(HttpClientManager::class, fn (Container $c) => new HttpClientManager());
+
+        $container->set(HttpClientInterface::class, fn (Container $c) => $c->get(HttpClientManager::class));
+    }
+
+    /**
+     * @throws ContainerException
+     */
+    public function init(Container $container): object
+    {
+        return $container->get(HttpClientInterface::class);
+    }
+}
