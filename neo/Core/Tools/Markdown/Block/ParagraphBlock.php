3ad0536e99526ec5b3a93501decd5bd0e9652f3b
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-01 15:59:13 +0200

    feat: add ParagraphBlock to represent paragraph blocks

---
+++ neo/Core/Tools/Markdown/Block/ParagraphBlock.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Neo\Core\Tools\Markdown\Block;
+
+class ParagraphBlock extends AbstractBlock
+{
+    public function __construct(
+        public string $text,
+    ) {
+        parent::__construct('paragraph');
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+}
