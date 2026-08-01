e882fbf312737b3e34134aeb289cfdceb356b2fd
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-01 15:59:21 +0200

    feat: add CodeBlock to represent fenced code blocks

---
+++ neo/Core/Tools/Markdown/Block/CodeBlock.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Neo\Core\Tools\Markdown\Block;
+
+class CodeBlock extends AbstractBlock
+{
+    public function __construct(
+        public string $language,
+        public string $content,
+    )
+    {
+        parent::__construct('code');
+    }
+
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+}
