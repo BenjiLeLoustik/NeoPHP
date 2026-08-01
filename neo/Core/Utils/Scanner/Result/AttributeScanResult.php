e31c8ff901c764559062dd71024ca037c888eef2
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-01 16:51:12 +0200

    feat: add AttributeScanResult DTO

---
+++ neo/Core/Utils/Scanner/Result/AttributeScanResult.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Neo\Core\Utils\Scanner;
+
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionParameter;
+use ReflectionProperty;
+
+class AttributeScanResult
+{
+    /**
+     * @param 'class'|'method'|'property'|'parameter' $type
+     * @param array<mixed> $arguments
+     * @param ReflectionClass<object>|ReflectionMethod|ReflectionProperty|ReflectionParameter $reflection
+     */
+    public function __construct(
+        private string $target,
+        private object $attribute,
+        private array $arguments,
+        private string $type,
+        private ReflectionClass|ReflectionMethod|ReflectionProperty|ReflectionParameter $reflection,
+    ) {
+    }
+
+    public function getTarget(): string
+    {
+        return $this->target;
+    }
+
+    public function getAttribute(): object
+    {
+        return $this->attribute;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @return 'class'|'method'|'property'|'parameter'
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return ReflectionClass<object>|ReflectionMethod|ReflectionProperty|ReflectionParameter
+     */
+    public function getReflection(): ReflectionClass|ReflectionMethod|ReflectionProperty|ReflectionParameter
+    {
+        return $this->reflection;
+    }
+}
