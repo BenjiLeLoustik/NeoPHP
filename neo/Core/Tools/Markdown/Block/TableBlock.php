8190a179d18494be331f2c7e573b707c322d52da
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-01 15:59:37 +0200

    feat: add TableBlock to represent markdown tables

---
+++ neo/Core/Tools/Markdown/Block/TableBlock.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Neo\Core\Tools\Markdown\Block;
+
+class TableBlock extends AbstractBlock
+{
+    /**
+     * @param list<string> $header
+     * @param list<list<string>> $rows
+     */
+    public function __construct(
+        public readonly array $header,
+        public readonly array $rows,
+    ) {
+        parent::__construct('table');
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function getHeader(): array
+    {
+        return $this->header;
+    }
+
+    /**
+     * @return list<string<string>>
+     */
+    public function getRows(): array
+    {
+        return $this->rows;
+    }
+
+}
