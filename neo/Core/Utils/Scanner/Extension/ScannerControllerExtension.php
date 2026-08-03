607978db1bda3a9170486d269f481e897432e460
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-03 21:50:11 +0200

    refactor: rename getScanner to getFileScanner in ScannerControllerExtension PHPDoc

--- neo/Core/Utils/Scanner/Extension/ScannerControllerExtension.php
+++ neo/Core/Utils/Scanner/Extension/ScannerControllerExtension.php
@@ -8,9 +8,11 @@ use Neo\Core\DI\Container;
 use Neo\Core\Extension\Attribute\Extension;
 use Neo\Core\Extension\Enum\ExtensionTypeEnum;
 use Neo\Core\Utils\Scanner\ScannerAttributeManager;
+use Neo\Core\Utils\Scanner\ScannerFileManager;
 
 /**
  * @method \Neo\Core\Utils\Scanner\ScannerAttributeManager getScanner(string $classname)
+ * @method \Neo\Core\Utils\Scanner\ScannerFileManager getFileScanner();
  */
 #[Extension(type: ExtensionTypeEnum::CONTROLLER)]
 class ScannerControllerExtension implements ControllerExtensionInterface
@@ -18,5 +20,6 @@ class ScannerControllerExtension implements ControllerExtensionInterface
     public function extend(AbstractController $controller, Container $container): void
     {
         $controller->registerMethod('getScanner', fn(string $className) => new ScannerAttributeManager($className));
+        $controller->registerMethod('getFileScanner', fn() => new ScannerFileManager());
     }
 }
