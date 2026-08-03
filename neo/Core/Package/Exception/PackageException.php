f4df0c400e54013a5e4eeec8aab94376c6412fac
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-03 17:46:24 +0200

    feat: add PackageException

---
+++ neo/Core/Package/Exception/PackageException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neo\Core\Package\Exception;
+
+use Neo\Core\Error\Exception\FrameworkException;
+
+class PackageException extends FrameworkException
+{}
