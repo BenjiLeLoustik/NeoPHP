7803d901cefd13aedcbf22e68cb48ef95bcdcac5
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-04 18:03:57 +0200

    feat: add PackageInterface contract

---
+++ neo/Core/Package/Interface/PackageInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neo\Core\Package\Interface;
+
+use Neo\Core\DI\Container;
+
+interface PackageInterface
+{
+    public function getName(): string;
+
+    public function getPath(): string;
+
+    public function getControllerPath(): ?string;
+
+    public function getViewPath(): ?string;
+
+    public function getListenersPath(): ?string;
+
+    public function getCronsPath(): ?string;
+
+    public function getCommandsPath(): ?string;
+
+    public function getMigrationsPath(): ?string;
+
+    public function getConfigPath(): ?string;
+
+    public function register(Container $container): void;
+}
