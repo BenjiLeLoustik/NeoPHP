26cb52705eeba6d3b7073039269e370d9ec949b3
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-04 18:04:26 +0200

    feat: add PackageModule to register and wire declared packages

---
+++ neo/Core/Package/PackageModule.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Neo\Core\Package;
+
+use Neo\Core\DI\Container;
+use Neo\Core\Module\Interface\ModuleInterface;
+use Neo\Core\Package\Interface\PackageInterface;
+use Neo\Core\Utils\Config\ConfigModule;
+
+class PackageModule implements ModuleInterface
+{
+
+    /**
+     * @return list<class-string>
+     */
+    public function dependencies(): array
+    {
+        return [
+            ConfigModule::class,
+        ];
+    }
+
+    public function register(Container $container): void
+    {
+        $container->set('packages', function (Container $container): array {
+            $config = $container->get('package.configModule');
+
+            /** @var array<int, class-string<PackageInterface>> $packageClasses */
+            $packageClasses = $config->from('app')
+                ->get('packages', []);
+
+            $packages = [];
+
+            foreach ($packageClasses as $packageClass) {
+                /** @var PackageInterface $package */
+                $package = new $packageClass();
+                $package->register($container);
+
+                $configPath = $package->getConfigPath();
+                if ($configPath !== null) {
+                    PackageManager::copyConfigDefaults(
+                        $configPath,
+                        $container->get('appPath'),
+                        $package->getName()
+                    );
+                }
+
+                $packages[] = $package;
+            }
+
+            return $packages;
+        });
+    }
+
+    public function init(Container $container): object
+    {
+        /** @var array<int, PackageInterface> $packages */
+        $packages = $container->get('packages');
+
+        return (object)['packages' => $packages];
+    }
+}
