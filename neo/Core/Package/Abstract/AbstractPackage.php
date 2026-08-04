8ab704947da6ebf53f25fe670bfb52613684badb
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-04 18:04:13 +0200

    feat: add AbstractPackage with convention-based path resolution

---
+++ neo/Core/Package/Abstract/AbstractPackage.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Neo\Core\Package\Abstract;
+
+use Neo\Core\DI\Container;
+use Neo\Core\Package\Interface\PackageInterface;
+
+class AbstractPackage implements PackageInterface
+{
+
+    public function getName(): string
+    {
+        return '';
+    }
+
+    public function getPath(): string
+    {
+        return '';
+    }
+
+    public function getControllerPath(): ?string
+    {
+        return $this->resolve('src/Controllers');
+    }
+
+    public function getViewPath(): ?string
+    {
+        return $this->resolve('src/Templates');
+    }
+
+    public function getListenersPath(): ?string
+    {
+        return $this->resolve('src/Listeners');
+    }
+
+    public function getCronsPath(): ?string
+    {
+        return $this->resolve('src/Crons');
+    }
+
+    public function getCommandsPath(): ?string
+    {
+        return $this->resolve('src/Commands');
+    }
+
+    public function getMigrationsPath(): ?string
+    {
+        return $this->resolve('database/Migrations');
+    }
+
+    public function getConfigPath(): ?string
+    {
+        return $this->resolve('config');
+    }
+
+    public function register(Container $container): void
+    {}
+
+    private function resolve(string $relativePath): ?string
+    {
+        $path = rtrim($this->getPath(), '/\\') . '/' . $relativePath;
+
+        return is_dir($path) ? $path : null;
+    }
+}
