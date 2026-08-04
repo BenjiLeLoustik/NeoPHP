1969973fda29d6e42183465043bf0b3908c5865d
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-04 18:05:42 +0200

    feat: add PackageModule dependency to ConsoleModule

--- neo/Core/Console/ConsoleModule.php
+++ neo/Core/Console/ConsoleModule.php
@@ -6,13 +6,16 @@ namespace Neo\Core\Console;
 use Neo\Core\DI\Container;
 use Neo\Core\DI\Exception\ContainerException;
 use Neo\Core\Module\Interface\ModuleInterface;
+use Neo\Core\Package\PackageModule;
 
 class ConsoleModule implements ModuleInterface
 {
     /** @return list<class-string> */
     public function dependencies(): array
     {
-        return [];
+        return [
+            PackageModule::class
+        ];
     }
 
     public function register(Container $container): void
