3cef2854e9e92fafb2d872b120ace778f9525991
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-24 15:12:03 +0200

    feat: add CacheModule dependency to AuthModule for remember-me support

--- neo/Core/Security/Auth/AuthModule.php
+++ neo/Core/Security/Auth/AuthModule.php
@@ -8,6 +8,7 @@ use Neo\Core\DI\Container;
 use Neo\Core\DI\Exception\ContainerException;
 use Neo\Core\Http\Client\ClientModule;
 use Neo\Core\Module\Interface\ModuleInterface;
+use Neo\Core\Utils\Cache\CacheModule;
 use Neo\Core\Utils\Config\ConfigModule;
 
 class AuthModule implements ModuleInterface
@@ -19,7 +20,8 @@ class AuthModule implements ModuleInterface
     {
         return [
             ConfigModule::class,
-            ClientModule::class
+            ClientModule::class,
+            CacheModule::class,
         ];
     }
 
