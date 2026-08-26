0c14baca14caf8012e47eb362d8231884dc3babf
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-08-26 04:42:36 +0200

    feat: add NumberHelper with compact method

---
+++ neo/Core/Tools/Helper/Number/NumberHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Neo\Core\Tools\Helper\Number;
+
+class NumberHelper
+{
+    public static function compact(int|float $number, int $precision = 1): string
+    {
+        $sign = $number < 0 ? '-' : '';
+        $abs = abs($number);
+
+        $units = [
+            1_000_000_000 => 'B',
+            1_000_000 => 'M',
+            1_000 => 'K',
+        ];
+
+        foreach ($units as $threshold => $suffix) {
+            if ($abs >= $threshold) {
+                $divided = $abs / $threshold;
+                $factor = 10 ** $precision;
+                $truncated = floor($divided * $factor) / $factor;
+
+                $formatted = number_format($truncated, $precision, '.', '');
+                $formatted = rtrim(rtrim($formatted, '0'), '.');
+
+                return $sign . $formatted . $suffix;
+            }
+        }
+
+        return $sign . $abs;
+    }
+}
